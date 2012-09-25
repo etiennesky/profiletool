2a49ea9ac3813537215b70556d61a9245614f97f
Etienne Tourigny <etourigny.dev@gmail.com>
2012-09-24 22:34:51 -0300

    ui cleanup - all widgets in designer

--- ui/ui_ptdockwidget.py
+++ ui/ui_ptdockwidget.py
@@ -75,11 +75,7 @@ class Ui_PTDockWidget(QDockWidget,Ui_ProfileTool):
 		self.setMinimumSize(minsize)
 		self.setMaximumSize(maxsize)
 		self.iface.mapCanvas().setRenderFlag(False)
-		"""#Print button thing
-		if QT_VERSION >= 0X040100:
-			self.butPDF.setEnabled(True)
-		if QT_VERSION >= 0X040300:
-			self.butSVG.setEnabled(True)"""
+
 		#TableWiew thing
 		self.tableView.setModel(self.mdl)
 		self.tableView.setColumnWidth(0, 20)
@@ -90,9 +86,6 @@ class Ui_PTDockWidget(QDockWidget,Ui_ProfileTool):
 		self.tableView.setColumnHidden(4 , True)
 		self.mdl.setHorizontalHeaderLabels(["","","Layer","Band"])
 		#self.checkBox.setEnabled(False)
-		
-		self.verticalLayout_plot = QVBoxLayout(self.frame_for_plot)
-		self.verticalLayout_plot.setMargin(0)
 		
 		#The ploting area
 		self.plotWdg = None
@@ -113,100 +106,37 @@ class Ui_PTDockWidget(QDockWidget,Ui_ProfileTool):
 
 	def closeEvent(self, event):
 		self.emit( SIGNAL( "closed(PyQt_PyObject)" ), self )
+                QObject.disconnect(self.butPrint, SIGNAL("clicked()"), self.outPrint)
+                QObject.disconnect(self.butPDF, SIGNAL("clicked()"), self.outPDF)
+                QObject.disconnect(self.butSVG, SIGNAL("clicked()"), self.outSVG)
 		return QDockWidget.closeEvent(self, event)
 
 
 	def addPlotWidget(self, library):
 		layout = self.frame_for_plot.layout()
-		
-		
-		while 1:
-			child = layout.takeAt(0)
-			print str(child)
-			if not child:
-				break
-			if child.widget():
-				child.widget().deleteLater()
-			if child.layout():
-				QObject.disconnect(self.butPrint, SIGNAL("clicked()"), self.outPrint)
-				QObject.disconnect(self.butPDF, SIGNAL("clicked()"), self.outPDF)
-				QObject.disconnect(self.butSVG, SIGNAL("clicked()"), self.outSVG)
-				while 1:
-					child1 = child.layout().takeAt(0)
-					print str(child1)
-					if not child1:
-						break
-					if child1.widget():	
-						child1.widget().deleteLater()
-				child.layout().deleteLater()				
-
-		
-		
+		while layout.count():
+                        child = layout.takeAt(0)
+                        child.widget().deleteLater()
 		if library == "Qwt5":
+                        self.widget_save_buttons.setVisible( True )
 			self.plotWdg = PlottingTool().changePlotWidget("Qwt5", self.frame_for_plot)
-			self.verticalLayout_plot.addWidget(self.plotWdg)
-			
-			
-			self.butPrint = QPushButton(self.frame_for_plot)
-			sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Preferred)
-			sizePolicy.setHorizontalStretch(0)
-			sizePolicy.setVerticalStretch(0)
-			sizePolicy.setHeightForWidth(self.butPrint.sizePolicy().hasHeightForWidth())
-			self.butPrint.setSizePolicy(sizePolicy)
-			self.butPrint.setMinimumSize(QSize(10, 20))
-			self.butPrint.setText(QApplication.translate("ProfileTool", "Print", None, QApplication.UnicodeUTF8))
-			self.butPrint.setObjectName(("butPrint"))
-			#self._26.addWidget(self.butPrint)
-			self.butPDF = QPushButton(self.frame_for_plot)
-			self.butPDF.setEnabled(False)
-			sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Preferred)
-			sizePolicy.setHorizontalStretch(0)
-			sizePolicy.setVerticalStretch(0)
-			sizePolicy.setHeightForWidth(self.butPDF.sizePolicy().hasHeightForWidth())
-			self.butPDF.setSizePolicy(sizePolicy)
-			self.butPDF.setMinimumSize(QSize(10, 20))
-			self.butPDF.setText(QApplication.translate("ProfileTool", "Save as PDF", None, QApplication.UnicodeUTF8))
-			self.butPDF.setObjectName(("butPDF"))
-			#self._26.addWidget(self.butPDF)
-			self.butSVG = QPushButton(self.frame_for_plot)
-			self.butSVG.setEnabled(False)
-			sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Preferred)
-			sizePolicy.setHorizontalStretch(0)
-			sizePolicy.setVerticalStretch(0)
-			sizePolicy.setHeightForWidth(self.butSVG.sizePolicy().hasHeightForWidth())
-			self.butSVG.setSizePolicy(sizePolicy)
-			self.butSVG.setMinimumSize(QSize(10, 20))
-			self.butSVG.setText(QApplication.translate("ProfileTool", "Save as SVG", None, QApplication.UnicodeUTF8))
-			self.butSVG.setObjectName(("butSVG"))
-			#self._26.addWidget(self.butSVG)
-
-			self.horizontalLayout_tool = QHBoxLayout()
-			self.horizontalLayout_tool.setMargin(0)
-			self.verticalLayout_plot.addLayout(self.horizontalLayout_tool)
-			
-			self.horizontalLayout_tool.addWidget(self.butPrint)
-			self.horizontalLayout_tool.addWidget(self.butPDF)
-			self.horizontalLayout_tool.addWidget(self.butSVG)
-			
-			#Print button thing
+			layout.addWidget(self.plotWdg)
+						
 			if QT_VERSION >= 0X040100:
 				self.butPDF.setEnabled(True)
 			if QT_VERSION >= 0X040300:
 				self.butSVG.setEnabled(True)
 
-			
 			QObject.connect(self.butPrint, SIGNAL("clicked()"), self.outPrint)
-			QObject.connect(self.butPDF, SIGNAL("clicked()"), self.outPDF)
-			QObject.connect(self.butSVG, SIGNAL("clicked()"), self.outSVG)
-			
-			
-			
-			
-		if library == "Matplotlib":
+                        QObject.connect(self.butPDF, SIGNAL("clicked()"), self.outPDF)
+                        QObject.connect(self.butSVG, SIGNAL("clicked()"), self.outSVG)
+                                
+		elif library == "Matplotlib":
+                        self.widget_save_buttons.setVisible( False )
 			self.plotWdg = PlottingTool().changePlotWidget("Matplotlib", self.frame_for_plot)
-			self.verticalLayout_plot.addWidget(self.plotWdg)
+			layout.addWidget(self.plotWdg)
 			mpltoolbar = matplotlib.backends.backend_qt4agg.NavigationToolbar2QTAgg(self.plotWdg, self.frame_for_plot)
-			self.verticalLayout_plot.addWidget( mpltoolbar )
+			layout.addWidget( mpltoolbar )
 			lstActions = mpltoolbar.actions()
 			mpltoolbar.removeAction( lstActions[ 7 ] )
 			mpltoolbar.removeAction( lstActions[ 8 ] )
