41c5577aa9481383d25f89c315979cea61d659b4
Etienne Tourigny <etourigny.dev@gmail.com>
2013-06-13 18:26:59 -0300

    add matplotlib info in dialog about

--- ui/dlgabout.py
+++ ui/dlgabout.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'ui/dlgabout.ui'
 #
-# Created: Sun Apr  1 09:44:20 2012
-#      by: PyQt4 UI code generator 4.8.5
+# Created: Thu Jun 13 18:23:48 2013
+#      by: PyQt4 UI code generator 4.9.3
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -18,7 +18,6 @@ class Ui_DlgAbout(object):
     def setupUi(self, DlgAbout):
         DlgAbout.setObjectName(_fromUtf8("DlgAbout"))
         DlgAbout.resize(572, 274)
-        DlgAbout.setWindowTitle(QtGui.QApplication.translate("DlgAbout", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
         self.gridLayout = QtGui.QGridLayout(DlgAbout)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
         self.title = QtGui.QLabel(DlgAbout)
@@ -33,7 +32,6 @@ class Ui_DlgAbout(object):
         font.setBold(True)
         font.setWeight(75)
         self.title.setFont(font)
-        self.title.setText(QtGui.QApplication.translate("DlgAbout", "Profile Tool", None, QtGui.QApplication.UnicodeUTF8))
         self.title.setObjectName(_fromUtf8("title"))
         self.gridLayout.addWidget(self.title, 0, 1, 1, 1)
         self.description = QtGui.QLabel(DlgAbout)
@@ -45,7 +43,6 @@ class Ui_DlgAbout(object):
         font = QtGui.QFont()
         font.setPointSize(10)
         self.description.setFont(font)
-        self.description.setText(QtGui.QApplication.translate("DlgAbout", "$PLUGIN_DESCRIPTION$", None, QtGui.QApplication.UnicodeUTF8))
         self.description.setWordWrap(True)
         self.description.setObjectName(_fromUtf8("description"))
         self.gridLayout.addWidget(self.description, 1, 1, 1, 1)
@@ -53,7 +50,7 @@ class Ui_DlgAbout(object):
         self.scrollArea_2.setWidgetResizable(True)
         self.scrollArea_2.setObjectName(_fromUtf8("scrollArea_2"))
         self.scrollAreaWidgetContents_2 = QtGui.QWidget()
-        self.scrollAreaWidgetContents_2.setGeometry(QtCore.QRect(0, 0, 535, 303))
+        self.scrollAreaWidgetContents_2.setGeometry(QtCore.QRect(0, 0, 536, 303))
         self.scrollAreaWidgetContents_2.setObjectName(_fromUtf8("scrollAreaWidgetContents_2"))
         self.verticalLayout_3 = QtGui.QVBoxLayout(self.scrollAreaWidgetContents_2)
         self.verticalLayout_3.setObjectName(_fromUtf8("verticalLayout_3"))
@@ -67,25 +64,6 @@ class Ui_DlgAbout(object):
         self.label_3.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
         self.label_3.setFocusPolicy(QtCore.Qt.NoFocus)
         self.label_3.setFrameShape(QtGui.QFrame.NoFrame)
-        self.label_3.setText(QtGui.QApplication.translate("DlgAbout", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'Sans Serif\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Profile Tool Plugin</p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">The Profile Plugin plots terrain profiles along interactive pointed or selected polylines. It handles  any format supported by QGis. You can compare layers together, control height scale and line colors. Unfortunately it requires Qwt5 library.</p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Please send your reflections, opinions, suggestions and wishes (especially related to this plugin;) on <a href=\"http://hub.qgis.org/projects/profiletool/issues\"><span style=\" text-decoration: underline; color:#0000ff;\">http://hub.qgis.org/projects/profiletool/issues</span></a></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">If you find this plugin useful please don\'t hesitate to let me know desired improvements!</p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Profile Tool Plugin - License GNU GPL 2</p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Written in 2008 by Borys Jurgiel</p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Written in 2012 by Borys Jurgiel, Patrice Verchere</p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">REQUIREMENTS:</p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">- Qwt5 (python-qwt5-qt4)</p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">- QT ver 4.1 for saving to PDF and 4.3 for saving to SVG</p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
         self.label_3.setTextFormat(QtCore.Qt.AutoText)
         self.label_3.setScaledContents(False)
         self.label_3.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
@@ -101,5 +79,26 @@ class Ui_DlgAbout(object):
         QtCore.QMetaObject.connectSlotsByName(DlgAbout)
 
     def retranslateUi(self, DlgAbout):
-        pass
+        DlgAbout.setWindowTitle(QtGui.QApplication.translate("DlgAbout", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
+        self.title.setText(QtGui.QApplication.translate("DlgAbout", "Profile Tool", None, QtGui.QApplication.UnicodeUTF8))
+        self.description.setText(QtGui.QApplication.translate("DlgAbout", "$PLUGIN_DESCRIPTION$", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_3.setText(QtGui.QApplication.translate("DlgAbout", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Sans\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Sans Serif\';\">Profile Tool Plugin</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'Sans Serif\';\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Sans Serif\';\">The Profile Plugin plots terrain profiles along interactive pointed or selected polylines. It handles  any format supported by QGis. You can compare layers together, control height scale and line colors. Unfortunately it requires Qwt5 library.</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'Sans Serif\';\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Sans Serif\';\">Please send your reflections, opinions, suggestions and wishes (especially related to this plugin;) on </span><a href=\"http://hub.qgis.org/projects/profiletool/issues\"><span style=\" font-family:\'Sans Serif\'; text-decoration: underline; color:#0000ff;\">http://hub.qgis.org/projects/profiletool/issues</span></a></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Sans Serif\';\">If you find this plugin useful please don\'t hesitate to let me know desired improvements!</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'Sans Serif\';\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Sans Serif\';\">Profile Tool Plugin - License GNU GPL 2</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Sans Serif\';\">Written in 2008 by Borys Jurgiel</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Sans Serif\';\">Written in 2012 by Borys Jurgiel, Patrice Verchere</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'Sans Serif\';\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Sans Serif\';\">REQUIREMENTS:</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Sans Serif\';\">- Qwt5 (python-qwt5-qt4) and/or python-matplotlib</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Sans Serif\';\">- QT ver 4.1 for saving to PDF and 4.3 for saving to SVG</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'Sans Serif\';\"><br /></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
 
