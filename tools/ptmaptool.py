36a0805c54ee3196a3e7d5c567e006cfd4765d73
Borys Jurgiel <info@borysjurgiel.pl>
2013-11-04 15:44:50 +0100

    Restore commit 32f9fc1: Uncheck toolbar button when closing plugin (#6679)

--- tools/ptmaptool.py
+++ tools/ptmaptool.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 #-----------------------------------------------------------
-# 
+#
 # Profile
 # Copyright (C) 2008  Borys Jurgiel
 # Copyright (C) 2012  Patrice Verchere
 #-----------------------------------------------------------
-# 
+#
 # licensed under the terms of GNU GPL 2
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, print to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -59,8 +59,8 @@ class ProfiletoolMapTool(QgsMapTool):
 
 	def deactivate(self):
 		self.emit( SIGNAL("deactivate") )
-		#self.button.setCheckable(False)
-		#QgsMapTool.deactivate(self)
+		self.button.setCheckable(False)
+		QgsMapTool.deactivate(self)
 
 
 	def isZoomTool(self):
